c05abd748aefe3fd8d6b9eda74d9645ee79677ae
Diestro Corleone <matias.perez.mcd@gmail.com>
2021-10-06 08:56:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import HolaMundo, {AdiosMundo} from './components/HolaMundo';
 import Saludar from './components/Saludar';
 
 function App() {
+  const user = {
+    name: "Diestro Corleone",
+    edad: 32,
+    color: "Ultraviolet"
+  };
+  
   return (
     <main>
       <HolaMundo />
       <AdiosMundo />
-      <Saludar name="Diestro Corleone" edad="32" />
+      <Saludar name={userName} edad={edad} />
     </main>
   );
 }
